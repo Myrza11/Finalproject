926b661a87329ea24ff6e1fc0d2e422f8f2c736b
Myrza11 <bapaevmyrza038@gmail.com>
2023-11-22 22:46:17 +0600

    swagger

--- Oleo/Oleo/urls.py
+++ Oleo/Oleo/urls.py
@@ -18,22 +18,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls.conf import re_path
 
+
 from rest_framework import permissions
+from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.urls import re_path
+
+...
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -41,5 +44,8 @@ urlpatterns = [
     path('api/v1/food/', include('dishesls.urls')),
     path('api/v1/gpt/', include('gptchat.urls')),
     path('api/v1/comments/', include('commentslike.urls')),
-    path('api/v1/regauth/', include('regauth.urls'))
+    path('api/v1/regauth/', include('regauth.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
