2d8601213379627f625c8a2547a2d754a9103be9
Myrza11 <bapaevmyrza038@gmail.com>
2023-11-23 09:01:39 +0600

    удалил ластнейм

--- Oleo/regauth/serializers.py
+++ Oleo/regauth/serializers.py
@@ -7,10 +7,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
     password_confirm = serializers.CharField(write_only=True)
-    last_name = serializers.CharField(
-        required=False,
-        validators=[RegexValidator(regex='^[a-zA-Z]*$', message='Only letters are allowed.')]
-    )
     first_name = serializers.CharField(
         validators=[RegexValidator(regex='^[a-zA-Z]*$', message='Only letters are allowed.')]
     )
@@ -29,7 +25,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = CustomUsers.objects.create_user(
             username=validated_data['username'],
-            last_name=validated_data.get('last_name', ''),
             email=validated_data.get('email', ''),
             first_name=validated_data['first_name'],
             password=validated_data['password'],
